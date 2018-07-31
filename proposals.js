04e20431df8367c8560159485b617f372befc52d
Nuno <nm@ledgerassets.com>
2018-08-01 00:00:32 +0800

    Renamed votes to proposals

--- proposals.js
+++ proposals.js
@@ -22,24 +22,24 @@ var getTimestamp = function() {
 	return Math.floor(new Date().getTime()/1000);
 }
 
-// TODO: Validate vote data server side
+// TODO: Validate proposal data server side
 io.on('connection', function(socket){
 
-    socket.on('vote', function(vote){
-        vote["status"] = "pending";
-        vote["likes"] = "0";
-        vote["time"] = getTimestamp();
+    socket.on('proposal', function(proposal){
+        proposal["status"] = "pending";
+        proposal["likes"] = "0";
+        proposal["time"] = getTimestamp();
 
-        var query = con.query('INSERT INTO votes SET ?', vote, function (error, results, fields) {
+        var query = con.query('INSERT INTO proposals SET ?', proposal, function (error, results, fields) {
             if (error) throw error;
 
-            io.emit('vote', vote);
+            io.emit('proposal', proposal);
         });
     });
 
     socket.on('joined', function (user) {
         // Send chat history to user
-        con.query("SELECT * FROM votes ORDER BY time DESC LIMIT " + historyCount, function (error, results) {
+        con.query("SELECT * FROM proposals ORDER BY time DESC LIMIT " + historyCount, function (error, results) {
             if (error) throw error;
 
             socket.emit('history', results);
@@ -47,19 +47,19 @@ io.on('connection', function(socket){
     });
 
     socket.on('like', function (like) {
-        voteID = like["id"];
+        proposalID = like["id"];
 
-        con.query("SELECT * FROM votes WHERE id = ?", [voteID], function (error, results, fields) {
+        con.query("SELECT * FROM proposals WHERE id = ?", [proposalID], function (error, results, fields) {
             if (error) throw error;
 
             if (results.length > 0 && typeof results[0]!= 'undefined') {
-                var vote = results[0];
-                vote["likes"]++;
+                var proposal = results[0];
+                proposal["likes"]++;
 
-                con.query('UPDATE votes SET likes = ? WHERE id = ?', [vote["likes"], vote["id"]], function (error, results, fields) {
+                con.query('UPDATE proposals SET likes = ? WHERE id = ?', [proposal["likes"], proposal["id"]], function (error, results, fields) {
                     if (error) throw error;
                     
-                    io.emit('update_vote', vote);
+                    io.emit('update_proposal', proposal);
                 });
             }
         });
